f158fa8018cac02886efaa88766c9112644a96c1
santosh.ece06@gmail.com <santosh.ece06@gmail.com@f29e3eb8-5727-783e-2ccb-3b0e52e6505f>
2013-06-11 17:14:01 +0000

    spring intro but di

---
+++ src/in/async/spring/intro/Student.java
@@ -0,0 +1,14 @@
+package in.async.spring.intro;
+
+public class Student {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
