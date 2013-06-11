d4f3f2028a3b6181725c29bedaf15f89d99f1ba0
santosh.ece06@gmail.com <santosh.ece06@gmail.com@f29e3eb8-5727-783e-2ccb-3b0e52e6505f>
2013-06-11 17:13:35 +0000

    servlet 3 file upload demo

---
+++ trunk/src/in/async/specification3/demo/FileUploadDemo.java
@@ -0,0 +1,44 @@
+package in.async.specification3.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class FileUploadDemo
+ */
+@WebServlet("/FileUploadDemo")
+@MultipartConfig
+public class FileUploadDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Part part = request.getPart("file1");
+		System.out.println(part.getName());
+		part.write("C:/Users/santoshm/Desktop/file1.jpeg");
+	}
+
+}
